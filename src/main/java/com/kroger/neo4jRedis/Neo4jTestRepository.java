78b002b258061f228997b0d5e88d4a5e837268be
Suraj Adhikari <suraj.adhikari@kroger.com>
2018-02-28 07:58:04 -0500

    Code cleanup

--- src/main/java/com/kroger/neo4jRedis/Neo4jTestRepository.java
+++ src/main/java/com/kroger/neo4jRedis/Neo4jTestRepository.java
@@ -1,12 +1,10 @@
 package com.kroger.neo4jRedis;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
 
 @Repository
 interface Neo4jTestRepository extends GraphRepository<Movie> {
--- src/main/java/com/kroger/neo4jRedis/RestTestController.java
+++ src/main/java/com/kroger/neo4jRedis/RestTestController.java
@@ -2,24 +2,13 @@ package com.kroger.neo4jRedis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.exceptions.JedisConnectionException;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 
-import static sun.security.timestamp.TSResponse.BAD_REQUEST;
 
 @RestController
 public class RestTestController {
